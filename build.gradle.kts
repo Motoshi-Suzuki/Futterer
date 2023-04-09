7f8e72760cc8d6846cabddb791e2b3421d15b426
motoshi-suzuki <motoshi.suzukiii@gmail.com>
2023-04-09 12:42:13 +0900

    display sign

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,26 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+val skArtifact = "skiko-awt-runtime-macos-arm64"
+val skVersion = "0.7.58"
+val lwjglNatives = "natives-macos-arm64"
+val lwjglVersion = "3.3.2"
+
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    api("org.jetbrains.skiko:${skArtifact}:${skVersion}")
+    implementation("org.lwjgl:lwjgl:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-glfw:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-opengl:$lwjglVersion")
+    runtimeOnly("org.lwjgl:lwjgl:$lwjglVersion:$lwjglNatives")
+    runtimeOnly("org.lwjgl:lwjgl-glfw:$lwjglVersion:$lwjglNatives")
+    runtimeOnly("org.lwjgl:lwjgl-opengl:$lwjglVersion:$lwjglNatives")
 }
 
 tasks.test {
